9b81fa9f0e86c015e5436e392164c8d6204fd147
Skomoroha <oleg.skomoroha1@gmail.com>
2024-05-26 17:32:40 +0300

    lag6 configuring

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     }]
   ],
   port: 3030,
-  base: '/edu-db-cw-group1/',
+  base: '/edu-db-cw-group1Lab6Skomoroha/',
   theme: 'cool',
   // dest: 'dist',
   head: [
@@ -76,7 +76,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/Daniil-Dyachenko/edu-db-cw-group1',
+    repo: 'https://github.com/olehskomorokha/edu-db-cw-group1Lab6Skomoroha',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
